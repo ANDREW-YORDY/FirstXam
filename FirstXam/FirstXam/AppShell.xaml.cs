474993cc91195ae516f94c15895a470aac54fe1a
ANDREW-DELGADO <software.analyst.adr@gmail.com>
2023-09-02 11:47:45 -0500

    Subida del proyexto desde pc

---
+++ FirstXam/FirstXam/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using FirstXam.ViewModels;
+using FirstXam.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace FirstXam
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
---
+++ FirstXam/FirstXam/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using FirstXam.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace FirstXam.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
